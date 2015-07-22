ccf4cf5c067c5c880d77f8969f75f928630a2239
Micky Miseck <themiseck.rock@gmail.com>
2015-07-21 20:11:17 -0500

    keywords and description

--- extended_flatpages/admin.py
+++ extended_flatpages/admin.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
+from django import forms
 from django.contrib.flatpages.admin import FlatPageAdmin
 from django.contrib.flatpages.admin import FlatpageForm
-from django import forms
+from django.contrib.flatpages.models import FlatPage
 from django.utils.translation import ugettext_lazy as _
+from django.forms.models import model_to_dict
 
 from .models import CMSFlatPage
 
@@ -12,15 +14,19 @@ class CustomFlatpageForm(FlatpageForm):
     description = forms.CharField(required=False, widget=forms.Textarea, label=_(u'Description'))
     keywords = forms.CharField(required=False, widget=forms.Textarea, label=_(u'Keywords'))
 
-    def save(self, commit=True):
-        obj = super(CustomFlatpageForm, self).save(commit)
-        obj.save()
-        page = dict(
-            description=self.cleaned_data["description"],
-            keywords=self.cleaned_data["keywords"],
-        )
-        cmsflatpage = CMSFlatPage.objects.get_or_create(page=obj, defaults=page)
-        cmsflatpage.save()
+    def __init__(self, *args, **kwargs):
+        _instance = kwargs.get("instance", None)
+        if _instance:
+            try:
+                f = tuple(x.name for x in FlatPage._meta.fields)
+                kwargs["initial"] = model_to_dict(_instance.cmsflatpage, f)
+                kwargs["initial"].update(dict(
+                    keywords=_instance.cmsflatpage.keywords,
+                    description=_instance.cmsflatpage.description,
+                ))
+            except Exception as e:
+                print e
+        super(CustomFlatpageForm, self).__init__(*args, **kwargs)
 
 
 class CustomFlatPageAdmin(FlatPageAdmin): 
@@ -29,3 +35,16 @@ class CustomFlatPageAdmin(FlatPageAdmin):
         (None, {'fields': ('url', 'title', 'content', 'sites', 'keywords', 'description')}),
         (_('Advanced options'), {'classes': ('collapse',), 'fields': ('enable_comments', 'registration_required', 'template_name')}),
     )
+
+    def save_model(self, request, obj, form, change):
+        obj.save()
+        page = dict(
+            description=form.cleaned_data["description"],
+            keywords=form.cleaned_data["keywords"],
+        )
+        print page
+        cmsflatpage, b = CMSFlatPage.objects.get_or_create(page=obj, defaults=page)
+        if not b:
+            cmsflatpage.description = form.cleaned_data["description"]
+            cmsflatpage.keywords = form.cleaned_data["keywords"]
+        cmsflatpage.save()
